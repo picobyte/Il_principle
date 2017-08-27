66985c9321882ea2112dd97227581507f774d0e6
R <pico0obyte@gmail.com>
2017-08-28 00:09:46 +0200

    Create aspectratiopixmaplabel.h

---
+++ aspectratiopixmaplabel.h
@@ -0,0 +1,31 @@
+#ifndef ASPECTRATIOPIXMAPLABEL_H
+#define ASPECTRATIOPIXMAPLABEL_H
+
+#include <QLabel>
+#include <QPixmap>
+#include <QResizeEvent>
+#include <QGraphicsBlurEffect>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QPainter>
+
+class AspectRatioPixmapLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit AspectRatioPixmapLabel(QWidget *parent = 0);
+    virtual int heightForWidth( const int width ) const;
+    virtual QSize sizeHint() const;
+    QPixmap scaledPixmap() const;
+    void toggle_blur();
+public slots:
+    void setPixmap ( const QPixmap &);
+    void resizeEvent(QResizeEvent *);
+private:
+    QPixmap* pix;
+    QPixmap blur;
+    QPixmap orig;
+    bool is_blurred;
+};
+
+#endif // ASPECTRATIOPIXMAPLABEL_H
