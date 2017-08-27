4073dac7ba21c0a6f6c836f85eb04c1c2a75764e
R <pico0obyte@gmail.com>
2017-08-27 23:52:53 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * (c) Picobyte 2017, GPL2
+ */
+
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
