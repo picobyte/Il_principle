774b629e516af1de88b44c0a6ea0a36314ab1b2f
picobyte <pico0.o.byte@gmail.com>
2017-09-02 20:29:52 +0200

    recruft

--- convert_multi.pl
+++ convert_multi.pl
@@ -13,17 +13,66 @@ my %typex = (unsigned => 'toInt', int => 'toInt', bool => 'toBool', double => 't
 my $typexr = join("|", keys %typex);
 $typexr = qr/\b($typexr)\b/;
 
+my $created_constructor;
+
+my $class;
+my @vars;
+my $jl = "QJsonObject *d = NULL"; #json loadable
+sub create_constructor {
+    shift(@_) if @_;
+    if (@vars && $jl) {
+        shift(@_) if @_;
+        print OUT "\n\t$class($jl)\n\t{\n\t\tif (d) init(d);\n".
+        join("\n", @_)."\n\t}\n\tvoid init(QJsonObject *d)\n\t{\n";
+        print OUT "\t\tfor (QJsonObject::iterator it = d->begin(); it != d->end(); ++it) {\n";
+        my $else = "";
+        foreach my $el (@vars) {
+            if ($el =~ /(\w+) (\w+);/) {
+                if (exists $typex{$1}) {
+                    $el = "\t\t\t${else}__IF_VAR_FROM_JSON_AS(it, $2, $typex{$1})";
+                } elsif(exists $enums{$1}) {
+                    $el = "\t\t\t${else}__IF_ENUM_FROM_JSON_AS(it, $2, $1)";
+                } else {
+                    $el = "\t\t\t${else}__IF_OBJ_FROM_JSON_AS(it, $2)";
+                }
+            } elsif ($el =~ /QList\<(\w+)\> (\w+);/) {
+                if (exists $typex{$1}) {
+                    $el = "\t\t\t${else}__IF_LIST_FROM_JSON_TYPED(it, $2, $typex{$1})";
+                } elsif(exists $enums{$1}) {
+                    $el = "\t\t\t${else}__IF_LIST_FROM_JSON_ENUM(it, $2, $1)";
+                } else {
+                    $el = "\t\t\t${else}__IF_OBJLIST_FROM_JSON(it, $2, $1)";
+                }
+            } else {
+                $el =~ s/^(.*);$/\t\t\t\/\/$1/;
+                print OUT $el, "\n";
+                next;
+            }
+            $el =~ s/\bBodyPart\b/Body::Part/g;
+            $else = "else ";
+            print OUT $el, "\n";
+        }
+        print OUT "\t\t}\n\t}\n";
+        @vars = ();
+    } else {
+        print OUT "\n\t$class($jl)".(@_ ? "\n".join("\n", @_)."\n\t}\n" : " {}\n");
+    }
+    $created_constructor = 1;
+}
+
 my $C_H;
 while (my $f = shift) {
     if (not -e $f) {
-        open(OUT, ">$f") or die "$f$!";
+        if ($f =~ /-+nojso?n/i) {
+            $jl = "";
+        } else {
+            open(OUT, ">$f") or die "$f$!";
+        }
         next;
     }
     die unless openhandle(*OUT);
     open (IN, "<$f") or die "$f:$!";
-    my $class;
 
-    my @vars;
     my $mem_fun;
     my $in_get;
     my $in_set;
@@ -41,92 +90,63 @@ while (my $f = shift) {
             s/\bstring\b/QString/g;
             s/\bUtilityClass\.Clamp\(([^,(]+), ?([^,]+), ?([^,)]+)\)/$1 < $2 ? $2 : ($1 > $3 ? $3 : $1)/g;
             if ($in_get) {
-                if (s/^\t\t\t\}$//) {
-                    print OUT join("\n", @$in_get, "\t}")."\n";
+                die unless s/^\t\t//;
+                if (/^\t\}$/) {
+                    print OUT join("\n", @$in_get, $_)."\n";
                     $in_get = undef;
                 } else {
-                    s/^\t\t//;
                     push @$in_get, $_;
                 }
             } elsif ($in_set) {
-                if (s/^\t\t\t\}$//) {
+                die unless s/^\t\t//;
+                if (/^\t\}$/) {
                     print OUT join("\n", @$in_set, "\t}")."\n";
                     $in_set = undef;
                 } else {
-                    s/^\t\t//;
                     push @$in_set, $_;
                 }
             } elsif ($mem_fun) {
-                if (s/^\t\t\}$//) {
-                    if (@$mem_fun > 2) {
-                        $mem_fun->[0] = "\t".join(" ", @{$mem_fun->[0]});
-                        print OUT join("\n", @$mem_fun, "\t}")."\n";
+                die unless s/^\t//;
+                if (/^\t\}$/) {
+                    $mem_fun->[0] = "\t".join(" ", @{$mem_fun->[0]});
+                    if ($mem_fun->[0] =~ /^\t$class\(/) {
+                        create_constructor(@$mem_fun);
+                    } elsif (@$mem_fun > 2) {
+                        print OUT join("\n", @$mem_fun, $_)."\n";
                     }
                     $mem_fun = undef;
-                } elsif (s/^\t\t\tget(;?)$/\tconst $mem_fun->[0]->[0] get_$mem_fun->[0]->[1]() const$1/) {
+                } elsif ($mem_fun->[0]->[0] eq $class) { # costructor
+                    push (@$mem_fun, $_) unless / \= new \w+\<\w+\>\(\);/;
+                } elsif (s/^\t\tget(;?)$/\tconst $mem_fun->[0]->[0] get_$mem_fun->[0]->[1]() const$1/) {
                     if (s/;$/ {return $mem_fun->[0]->[1];}/) {
                         print OUT $_."\n";
                     } else {
                         $in_get = [$_];
                     }
-                } elsif (s/^\t\t\tset(;?)$/\tvoid set_$mem_fun->[0]->[1]($mem_fun->[0]->[0]\& v)$1/) {
+                } elsif (s/^\t\tset(;?)$/\tvoid set_$mem_fun->[0]->[1]($mem_fun->[0]->[0]\& v)$1/) {
                     if (s/;$/ {$mem_fun->[0]->[1] = v;}/) {
                         print OUT $_."\n";
                     } else {
                         $in_set = [$_];
                     }
                 } else {
-                    s/^\t//;
                     push @$mem_fun, $_;
                 }
-            } elsif (/^\t\}/) {
-                #create constructor;
-                if (@vars) {
-                    print OUT "\n\t$class(QJsonObject *d = NULL)\n\t{\n\t\tif (d) init(d);\n\t}\n\tvoid init(QJsonObject *d)\n\t{\n";
-                    print OUT "\t\tfor (QJsonObject::iterator it = d->begin(); it != d->end(); ++it) {\n";
-                    my $else = "";
-                    foreach my $el (@vars) {
-                        if ($el =~ /(\w+) (\w+);/) {
-                            if (exists $typex{$1}) {
-                                $el = "\t\t\t${else}__IF_VAR_FROM_JSON_AS(it, $2, $typex{$1})";
-                            } elsif(exists $enums{$1}) {
-                                $el = "\t\t\t${else}__IF_ENUM_FROM_JSON_AS(it, $2, $1)";
-                            } else {
-                                $el = "\t\t\t${else}__IF_OBJ_FROM_JSON_AS(it, $2)";
-                            }
-                        } elsif ($el =~ /QList\<(\w+)\> (\w+);/) {
-                            if (exists $typex{$1}) {
-                                $el = "\t\t\t${else}__IF_LIST_FROM_JSON_TYPED(it, $2, $typex{$1})";
-                            } elsif(exists $enums{$1}) {
-                                $el = "\t\t\t${else}__IF_LIST_FROM_JSON_ENUM(it, $2, $1)";
-                            } else {
-                                $el = "\t\t\t${else}__IF_OBJLIST_FROM_JSON(it, $2, $1)";
-                            }
-                        } else {
-                            $el =~ s/^(.*);$/\t\t\t\/\/$1/;
-                            print OUT $el, "\n";
-                            next;
-                        }
-                        $el =~ s/\bBodyPart\b/Body::Part/g;
-                        $else = "else ";
-                        print OUT $el, "\n";
-                    }
-                    print OUT "\t\t}\n\t}\n};";
-                    @vars = ();
-                } else {
-                    print OUT "\n\t$class(QJsonObject *d = NULL) {}\n};";
-                }
-            } elsif (s/^\t\t(private|public) (static)?(.*;)$/$3/) {
+            } elsif (/^\}/) { # namespace end occurs first, actually.
+                print OUT "};\n" and next if $created_constructor;
+                create_constructor();
+            } elsif (s/^\t\t(private|public) (static)? ?(.*;)$/$3/) {
                 s/ _/ /;
                 push @vars, $_;
                 print OUT "\t".$_, "\n";
-            } elsif (/^\t\t(private|public) (static|override)?(\w+) (\w+)$/) {
-                $mem_fun = [[$3, $4]];
+            } elsif (/^\t\t(private|public) (static|override)? ?((\w+ )*\w+\(.*)$/) {
+                $mem_fun = [[split(/ /, $3)]];
+
             }
         } elsif (/public(?: abstract)? class (\w+)/ and not $C_H) {
             $class = $1;
             $C_H = uc $class."_H";
-            print OUT "#ifndef $C_H\n#define $C_H\n#include \"json_macros.h\"\n\nclass $class {\npublic:\n";
+            print OUT "#ifndef $C_H\n#define $C_H\n".($jl ? "#include \"json_macros.h\"\n" : "")."\nclass $class {\npublic:\n";
         } elsif (/\tpublic class (\w+) : (\w+)/) {
             die "feed abstract class first!" if not $C_H;
             $class = $1;
@@ -139,5 +159,5 @@ while (my $f = shift) {
     }
     close IN;
 }
-print OUT "\n\n#endif // $C_H\n";
+print OUT "\n#endif // $C_H\n";
 close OUT;
